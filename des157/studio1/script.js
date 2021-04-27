de6f76654e0234b111fa7705d02804b329f181ae
Xinlei Li <xlzli@ucdavis.edu>
2021-04-27 09:58:46 -0700

    mad libs assignment

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -1,29 +1,41 @@
-(function(){
-    "use strict";
+(function () {
 
+    'use strict';
+
+    // open and close buttons
+    document.querySelector(".open").addEventListener("click", function(event){
+        event.preventDefault();
+        document.getElementById("overlay").className = "showing";
+    });
+    
+    // document.querySelector(".close").addEventListener("click", function(event){
+    //     event.preventDefault();
+    //     document.getElementById("overlay").className = "hidden";
+    // });
+
+    document.addEventListener("keydown", function(event){
+        if (event.key === "Escape"){
+            document.getElementById("overlay").className = "hidden";
+        }
+    });
+
+    // (1) users select their options and submit the form
     const myForm = document.querySelector("#myform");
+
+    // (2) after users finish their selections and press submit, madlib section will put into action
     const madlib = document.querySelector("#madlib");
 
     myForm.addEventListener("submit", function(event){
-
         event.preventDefault();
-        let noun1 = document.querySelector("#noun1").value;
-        let noun2 = document.querySelector("#noun2").value;
-        let adj1 = document.querySelector("#adj1").value;
-        let verb1 = document.querySelector("#verb1").value;
+        const mean = document.querySelector("#mean").value;
+        const want = document.querySelector("#want").value;
+        const balloon = document.querySelector("#balloon").value;
+        const like = document.querySelector("#like").value;
+        const act = document.querySelector("#act").value;
 
-        let myText;
-
-        if (noun1 && noun2 && adj1 && verb1) {
-            myText = `Here are the words: ${noun1}, ${noun2}, ${adj1}, ${verb1}`;
-        } else {
-            myText = "Please give me words so I can make your Mad Lib!";
-        }
-
-        // let myText = `Here are the words: ${noun1}, ${noun2}, ${adj1}, ${verb1}`;
+        const myText = `Balloons are the symbol of ${mean} to me. ${want} since when I was 8 years old. When I was in primary school, many classmates had balloons, and I was very envious. I asked my mom to buy one for me. The color was like  ${balloon}, which ${like} If I could have one more balloon, could I ${act}?`;
 
         madlib.innerHTML = myText;
-
     });
-    
-}())
+
+})();
