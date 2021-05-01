621195f02e420b8095e64d8f2b916522f0b2fc7c
Xinlei Li <xlzli@ucdavis.edu>
2021-05-01 02:46:57 -0700

    madlibs complete

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -6,24 +6,65 @@
     const myForm = document.querySelector("#myform");
     const madlib = document.querySelector("#madlib");
     const tryagain = document.querySelector("#tryagain");
-    const character = document.querySelector("#character");
+    const defaultCharacter = document.querySelector("#defaultCharacter");
+    const defaultBalloon = document.querySelector("#defaultBalloon");
 
     myForm.addEventListener("submit", function(event){
         event.preventDefault();
-        const mean = document.querySelector("#mean").value;
-        if (mean === "Happiness"){
-            const smile = document.querySelector("#characterSmile");
-            character.className = "hidden";
-            smile.className = "";
-        } else if (mean === "Nightmares") {
-            const nightmares = document.querySelector("#")
-        }
-        const want = document.querySelector("#want").value;
-        const balloon = document.querySelector("#balloon").value;
-        const like = document.querySelector("#like").value;
-        const act = document.querySelector("#act").value;
 
-        const myText = `Balloons are the symbol of ${mean} to me. ${want} since when I was 8 years old. When I was in primary school, many classmates had balloons, and I was very envious. I asked my mom to buy one for me. The color was like  ${balloon}, which ${like} If I could have one more balloon, could I ${act}?`;
+        const mean = document.querySelector("#mean").value;
+        if (mean === "nightmares. It is so vulnerable that could scare me at any time with loud bursting sound.") {
+            document.getElementById("madlib").style.color = "black";
+        }
+
+        const want = document.querySelector("#want").value;
+        if (want === "I've been asking God to keep me away from balloons"){
+            defaultBalloon.className = "hidden";
+        }
+
+        const balloon = document.querySelector("#balloon").value;
+        if (balloon === "ice-a passion fruit flavored ice-cream"){
+            const icecream = document.querySelector("#blIcecream");
+
+            icecream.className = "";
+        } else if (balloon === "my favorite Starbucks frappuccino") {
+            const starbucks = document.querySelector("#blStarbucks");
+
+            starbucks.className = "";
+        } else if (balloon === "a cup of milktea with boba") {
+            const milktea = document.querySelector("#blMilktea");
+
+            milktea.className = "";
+        }
+
+        const like = document.querySelector("#like").value;
+        if (like === "I loved so much."){
+            const smile = document.querySelector("#characterSmile");
+            defaultCharacter.className = "hidden";
+            smile.className = "";
+        } else if (like === "I hated so bad.") {
+            const sad = document.querySelector("#characterSad");
+            defaultCharacter.className = "hidden";
+            sad.className = "";
+        }
+        
+        const act = document.querySelector("#act").value;
+        if (act === "blow them up?"){
+            defaultBalloon.className = "hidden";
+            const blowup = document.querySelector("#blowup");
+            blowup.className = "";
+        } else if (act === "sned them to who I love?") {
+            const gift = document.querySelector("#gift");
+            defaultBalloon.className = "hidden";
+            gift.className = "";
+        }
+
+        // alert if there is empty selection
+        // if (mean || want || balloon || like || act === "empty") {
+        //     alert("You have an empty selection. Please try again :)");
+        // }
+
+        const myText = `Balloon means a lot to different people. For me, balloon is a symbol of ${mean} ${want} since I was little. I asked my mom to buy one for me. The style was like ${balloon}, which ${like} If I could have one more balloon, could I ${act}`;
 
         myForm.className = "hidden";
         madlib.innerHTML = myText;
