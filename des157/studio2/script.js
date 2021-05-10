0cdb563bfb3bb9823469a986fc310044f9ec6195
Xinlei Li <xlzli@ucdavis.edu>
2021-05-10 11:48:28 -0700

    delete zoom in effects and simply change into hover effects

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -1,36 +1,6 @@
 (function () {
     "use strict";
 
-    // try to learn from the mouse over zoom in exercise
-    const container = document.querySelector("#bg");
-    const hotSpots = document.querySelectorAll("#bg div");
-    const theImg = document.querySelector("div img");
-
-    hotSpots.forEach(function(eachSpot){
-        eachSpot.addEventListener("mouseover", square);
-        eachSpot.addEventListener("mouseout", function(){
-            theImg.className="start";
-        })
-    })
-
-    function square(event){
-        const thisCorner = event.target.id;
-        switch (thisCorner) {
-            case "topOne" : theImg.className = "topOne"; break;
-            case "topTwo" : theImg.className = "topTwo"; break;
-            case "topThree" : theImg.className = "topThree"; break;
-            case "topFour" : theImg.className = "topFour"; break;
-            case "topFive" : theImg.className = "topFive"; break;
-            case "bottomOne" : theImg.className = "bottomOne"; break;
-            case "bottomTwo" : theImg.className = "bottomTwo"; break;
-            case "bottomThree" : theImg.className = "bottomThree"; break;
-            case "bottomFour" : theImg.className = "bottomFour"; break;
-            case "bottomFive" : theImg.className = "bottomFive"; break;
-            case "bottomSix" : theImg.className = "bottomSix"; break;
-            case "bottomSeven" : theImg.className = "bottomSeven"; break;
-        }
-    }
-
     // try to do open overlay exercise
     const openBtns = document.querySelectorAll(".open");
     const closeBtns = document.querySelectorAll(".close");
