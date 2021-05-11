ba2816843042b1b0a60ec830dc0a5c2651e9c1d7
Xinlei Li <xlzli@ucdavis.edu>
2021-05-11 05:50:05 -0700

    every picture code

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -27,4 +27,44 @@
         }
     });
 
+    // slide images
+    const myImages = [
+        "slide1.jpg",
+        "slide2.jpg",
+        "slide3.jpg",
+        "slide4.jpg",
+        "slide5.jpg",
+        "slide6.jpg",
+        "slide7.jpg",
+        "slide8.jpg"
+    ];
+
+        let currentImage = 0;
+
+        const slide = document.getElementById("myimage");
+
+        document.getElementById("next").addEventListener("click", nextPhoto);
+
+        function nextPhoto (){
+            currentImage++;
+
+            if (currentImage > myImages.length - 1) {
+                currentImage = 0;
+            }
+
+            slide.src = `images/${myImages[currentImage]}`;
+        }
+
+        document.getElementById("previous").addEventListener("click", previousPhoto);
+
+        function previousPhoto (){
+            currentImage--;
+
+            if (currentImage < 0){
+                currentImage = myImages.length-1;
+            }
+
+            slide.src = `images/${myImages[currentImage]}`;
+        }
+
 })();
